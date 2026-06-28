6e0eae50b51e409633585cff232280bab7c78220
Alexleop <lx.leopoldo@outlook.com>
2026-06-27 22:36:11 -0300

    fix(cors): allow x-user-role header

--- config/settings.py
+++ config/settings.py
@@ -61,6 +61,11 @@ CORS_ALLOWED_ORIGINS = [
 ]
 CORS_ALLOW_CREDENTIALS = True
 
+from corsheaders.defaults import default_headers
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    "x-user-role",
+]
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
